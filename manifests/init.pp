a3e9695d9443e6c2a098e4d2685aaffcbd2020ef
robbevaes2 <robbe.vaes99@gmail.com>
2022-09-14 13:13:05 +0200

    [Everesst] Add bookkeeper/zookeeper newrelic config

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,14 @@
 # Sample Usage:
 #
 class bookkeeper(
-  $service_enable = $::bookkeeper::params::service_enable,
-  $service_ensure = $::bookkeeper::params::service_ensure,
-  $service_manage = $::bookkeeper::params::service_manage,
-  $service_name   = $::bookkeeper::params::service_name,
-  $config_name    = $::bookkeeper::params::config_name,
-  $log_dir        = $::bookkeeper::params::log_dir,
-  $logger         = $::bookkeeper::params::logger
+  $service_enable        = $::bookkeeper::params::service_enable,
+  $service_ensure        = $::bookkeeper::params::service_ensure,
+  $service_manage        = $::bookkeeper::params::service_manage,
+  $service_name          = $::bookkeeper::params::service_name,
+  $config_name           = $::bookkeeper::params::config_name,
+  $log_dir               = $::bookkeeper::params::log_dir,
+  $logger                = $::bookkeeper::params::logger,
+  $bookkeeper_extra_opts = undef
   ) inherits bookkeeper::params {
 
   contain 'bookkeeper::package'
